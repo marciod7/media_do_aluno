c9d35ae573a3ee24335e41a5e7d08d7ad182e762
marciod7 <marciod7@yahoo.com.br>
2024-10-11 13:57:38 -0300

    Atualização no arquivo [foi  adicionando mais um comando no caso o elif]

--- soma_nota.py
+++ soma_nota.py
@@ -1,8 +1,12 @@
 num = float(input("1ª nota do aluno: "))
 num2 = float(input("2ª nota do aluno: "))
 media = (num + num2) / 2
-print(f"A média da nota {num} + {num2} é {media:.2f}")
+print(f"A soma da nota de {num} + {num2} será sua média de {media:.2f}")
+print("=" * 40)
 if media >= 6:
     print("Parabéns. Você foi aprovada!")
+elif media >= 4 and media < 6:
+    print("Você está de recuperação, estude mais!")
 else:
-    print("Você foi reprovada. Estude mais!")
+    print("Você foi reprovado!")
+print("=" * 40)
